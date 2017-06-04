fad78a6759a837a7951779f8a9cbbf914acb1419
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-06-04 16:00:31 +0700

    Separate reducer to small reducer: LibraryReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import LibraryReducer from './LibraryReducer';
 
+// Use combineReducers because we expect to work with mutiple reducer and it must be nicely, works
+// well together
 export default combineReducers({
-  libraries: () => []
+  libraries: LibraryReducer
 });
