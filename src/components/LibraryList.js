1c6f132e566e2e833f015ecf709fa12091f9f2e1
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-06-05 21:31:00 +0700

    Setup listView

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
 
-  render() {
-    console.log(this.props);
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
 
-    return ;
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  renderRow() {
+    
+  }
+
+  render() {
+    return (
+      <ListView 
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      ></ListView>
+    );
   }
 }
 
 const mapStateToProps = state => {
-  return {libraries: state.libraries};
+  return { libraries: state.libraries };
 };
 
 // use connect to connect React and Redux
