6ef794710e8454862a7f26da93c396bcec91a5b1
Hai Pham <hai.phamthanh@vndirect.com.vn>
2017-06-05 21:52:19 +0700

    Add selectionReducer to our app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 // Use combineReducers because we expect to work with mutiple reducer and it must be nicely, works
 // well together
 export default combineReducers({
-  libraries: LibraryReducer
+  libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer
 });
